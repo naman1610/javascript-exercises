c4dc14c73984806533c4feb715660ae80fa0664c
Naman <naman16102003@gmail.com>
2022-06-26 19:45:24 +0530

    Completed removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...val) {
+    let finalArr = [...arr];
+    for (let i = 1; i < arguments.length; i++) {
+        finalArr = finalArr.filter((elem) => elem !== arguments[i]);
+    }
+    return finalArr;
 };
 
 // Do not edit below this line
