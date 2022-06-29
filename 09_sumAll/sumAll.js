0211ebe8f779ebaaeca07dbede0bd0ca00c561bb
Naman <naman16102003@gmail.com>
2022-06-29 18:52:30 +0530

    Completed sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(n1, n2) {
+    if (Number.isInteger(n1) && Number.isInteger(n2) && n1 > 0 && n2 > 0) {
+        if (n1 > n2) {
+            return calcSum(n1,n2);
+        } else {
+            return calcSum(n2,n1);
+        }
+    } else {
+        return 'ERROR';
+    }
 };
+
+function calcSum(high, low) {
+    let s = 0;
+    while (low <= high) {
+        s+=low;
+        low++;
+    }
+    return s;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
